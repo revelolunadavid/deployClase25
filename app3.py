211b08cfe6c62f1cd830ce5f0b05ab3bcfa63816
revelolunadavid <drevelo123@gmail.com>
2025-03-25 16:56:01 -0500

    config cors

--- app3.py
+++ app3.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from routers import routerDiabetes
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(routerDiabetes.router)
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes cambiar "*" por ["http://127.0.0.1:5500"] si quieres más seguridad
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 if __name__=="__main__":
     app.run()
